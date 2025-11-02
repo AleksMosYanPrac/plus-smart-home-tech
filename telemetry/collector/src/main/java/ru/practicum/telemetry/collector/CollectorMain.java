5b9f1feb4a2940db195065741f172e2cfddac4d8
Aleksandr <aliaksandrmo@yandex.ru>
2025-11-02 15:44:21 +0300

    fix: pipeline error

--- telemetry/collector/src/main/java/ru/practicum/telemetry/collector/CollectorMain.java
+++ telemetry/collector/src/main/java/ru/practicum/telemetry/collector/CollectorMain.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CollectorMain {
-
     public static void main(String[] args) {
         SpringApplication.run(CollectorMain.class, args);
     }
-
 }
